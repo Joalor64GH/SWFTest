105f8dcca51c03efae121da374ebcbeeb57ab487
Joalor64 <rhgarcon@gmail.com>
2024-04-22 10:02:46 -0400

    formatting

--- source/SWF.hx
+++ source/SWF.hx
@@ -1,81 +1,74 @@
 package;
 
-import openfl.utils.Assets;
-import openfl.display.Sprite;
-import openfl.Lib;
-import openfl.events.Event;
-import openfl.display.MovieClip;
-
 import flixel.FlxG;
 import flixel.sound.FlxSound;
 
-class SWF extends Sprite
-{
-    public var clip:MovieClip;
+import openfl.Lib;
+import openfl.Assets;
+import openfl.display.Sprite;
+import openfl.display.MovieClip;
+import openfl.events.Event;
 
-    private var barLeft:Sprite;
-    private var barRight:Sprite;
+class SWF extends Sprite {
+	public var clip:MovieClip;
 
-    public function new(movieClip:String, sound:String, onComplete:Void->Void):Void
-    {
-        super();
+	private var barLeft:Sprite;
+	private var barRight:Sprite;
 
-        barLeft = new Sprite();
-        barRight = new Sprite();
+	public function new(movieClip:String, sound:String, onComplete:Void->Void):Void {
+		super();
 
-        var audio:FlxSound = new FlxSound().loadEmbedded(sound);
-        Assets.loadLibrary('$movieClip').onComplete(function(_) {
-            clip = Assets.getMovieClip('$movieClip:');
-            addChild(clip);
+		barLeft = new Sprite();
+		barRight = new Sprite();
 
-            addChild(barLeft);
-            addChild(barRight);
+		var audio:FlxSound = new FlxSound().loadEmbedded(sound);
+		Assets.loadLibrary('$movieClip').onComplete((_) -> {
+			clip = Assets.getMovieClip('$movieClip:');
+			addChild(clip);
 
-            audio.onComplete = function() {
-                onComplete();
-                removeChild(clip);
-                (cast (Lib.current.getChildAt(0), Main)).removeChild(this);
-            };
+			addChild(barLeft);
+			addChild(barRight);
 
-            if (audio != null)
-                audio.play();
-        });
+			audio.onComplete = () -> {
+				onComplete();
+				removeChild(clip);
+				(cast(Lib.current.getChildAt(0), Main)).removeChild(this);
+			};
 
-        (cast (Lib.current.getChildAt(0), Main)).addChild(this);
+			if (audio != null)
+				audio.play();
+		});
 
-        addEventListener(Event.ENTER_FRAME, onResize);
-    }
+		(cast(Lib.current.getChildAt(0), Main)).addChild(this);
 
-    /**
-     *  Partly Copied from FlxGame
-     */
-    function onResize(_):Void
-    {
-        var width:Int = FlxG.stage.stageWidth;
-	    var height:Int = FlxG.stage.stageHeight;
+		addEventListener(Event.ENTER_FRAME, onResize);
+	}
 
-        if (clip != null)
-            width > height ? clip.scaleX = clip.scaleY = height / 720 : clip.scaleY = clip.scaleX = width / 1280;
+	function onResize(_):Void {
+		var width:Int = FlxG.stage.stageWidth;
+		var height:Int = FlxG.stage.stageHeight;
 
-        screenCenter();
-    }
+		if (clip != null)
+			width > height ? clip.scaleX = clip.scaleY = height / 720 : clip.scaleY = clip.scaleX = width / 1280;
 
-    public function screenCenter()
-    {
-        var ratio:Float = FlxG.width / FlxG.height;
-	    var realRatio:Float = FlxG.stage.stageWidth / FlxG.stage.stageHeight;
-        var preX:Float = 0;
+		screenCenter();
+	}
 
-        preX = Math.floor(FlxG.stage.stageHeight * ratio);
+	public function screenCenter() {
+		var ratio:Float = FlxG.width / FlxG.height;
+		var realRatio:Float = FlxG.stage.stageWidth / FlxG.stage.stageHeight;
+		var preX:Float = 0;
 
-        if (clip != null)
-		    clip.x = Math.ceil((FlxG.stage.stageWidth - preX) * 0.5);
+		preX = Math.floor(FlxG.stage.stageHeight * ratio);
 
-        barLeft.graphics.clear();
-        barRight.graphics.clear();
-        barLeft.graphics.beginFill();
-        barRight.graphics.beginFill();
-        barLeft.graphics.drawRect(0, 0, clip.x, FlxG.stage.stageHeight);
-        barRight.graphics.drawRect(FlxG.stage.stageWidth - clip.x, 0, clip.x, FlxG.stage.stageHeight);
-    }
+		if (clip != null)
+			clip.x = Math.ceil((FlxG.stage.stageWidth - preX) * 0.5);
+
+		barLeft.graphics.clear();
+		barRight.graphics.clear();
+		barLeft.graphics.beginFill();
+		barRight.graphics.beginFill();
+		barLeft.graphics.drawRect(0, 0, clip.x, FlxG.stage.stageHeight);
+		barRight.graphics.drawRect(FlxG.stage.stageWidth - clip.x, 0, clip.x, FlxG.stage.stageHeight);
+	}
 }
